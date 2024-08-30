92747e2b6aaa3c66abbf16b91eae88ede8b29c50
A97Campos <andrewleandro97@gmail.com>
2024-08-30 10:51:14 -0300

    criando a navegação entre páginas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Container, EstiloGlobal } from './styles'
 
 import store from './store'
 import Home from './pages/Home'
+import Cadastro from './pages/Cadastro'
 
 const rotas = createBrowserRouter([
   {
@@ -13,7 +14,7 @@ const rotas = createBrowserRouter([
   },
   {
     path: '/novo',
-    element: <h1>Nova Página</h1>
+    element: <Cadastro />
   }
 ])
 
